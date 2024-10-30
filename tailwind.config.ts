07cce573bc643435aabf54229f112f5ab87b0e0f
zainub-nazish <160176214+zainub-nazish@users.noreply.github.com>
2024-10-30 19:06:44 +0500

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import { access } from "fs";
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      container : {
+        center: true,
+        padding: "15px",
+      },
+      colors: {
+       accent: "#08E95E"
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
